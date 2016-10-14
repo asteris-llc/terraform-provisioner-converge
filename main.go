574dd96456a8c50f81e17e49a0ba7cd5ea2a72c0
Chris Aubuchon <Chris.Aubuchon@gmail.com>
2016-10-14 15:29:36 -0500

    Add version to main.go

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
+const Name = "terraform-provisioner-converge"
+const Version = "0.1.0"
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProvisionerFunc: func() terraform.ResourceProvisioner {
