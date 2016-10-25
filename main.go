b398adc391d6167989f283140629223adb0acd95
Steven Borrelli <steve@borrelli.org>
2016-10-25 14:37:09 -0500

    update repo location

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/ChrisAubuchon/terraform-provisioner-converge/converge"
+	"github.com/asteris-llc/terraform-provisioner-converge/converge"
 
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
